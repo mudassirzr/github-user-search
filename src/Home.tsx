74f925cb1f9a570049e7296d5f181017c3e2d263
Mudasir Zahoor <mudassirzahoor14@gmail.com>
2022-07-13 22:41:54 +0530

    small typo

--- src/Home.tsx
+++ src/Home.tsx
@@ -3,7 +3,7 @@ import SearchForm from "components/searchForm";
 function Home() {
   return (
     <div>
-      <h1 className={'homeHeaderText'}>Lookup Github User Details</h1>
+      <h1 className={'homeHeaderText'}>Github User Lookup</h1>
       <p className={'homeHeaderSubText'}>type a github username to check full details of the user!</p>
       <SearchForm />
     </div>
